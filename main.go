675c48f822d44c75ebc3012a77664862e4d7b82d
ToBeAss <tobiasmol@uia.no>
2023-04-14 03:49:56 +0200

    ryddet i fila

--- main.go
+++ main.go
@@ -1,56 +1,45 @@
 package main
 
 import (
-	"os"
 	"bufio"
+	"fmt"
 	"io"
 	"log"
-	"fmt"
-	"strings"
+	"os"
 	"strconv"
+	"strings"
+
 	"github.com/ToBeAss/funtemps/conv"
 )
 
-func main(){
-	src, err := os.Open("kjevik-temp-celsius-20220318-20230318.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer src.Close()
-
-	newFile, err := os.Create("kjevik-temp-fahr-20220318-20230318.csv")
-	if err != nil {
-    		log.Fatal(err)
-	}
-	defer newFile.Close()
-
+func main() {
 	var input string
-	var inputHistory []string
+	inputHistory := []string{"blank"}
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
-    		input = scanner.Text()
-    		if input == "q" || input == "exit" {
-    			inputHistory = append(inputHistory, input)
+		input = scanner.Text()
+		if input == "q" || input == "exit" {
+			inputHistory = append(inputHistory, input)
 			os.Exit(0)
-    		}
-		if len(inputHistory) > 0 && inputHistory[len(inputHistory)-1] == "convert" {
+		}
+		if inputHistory[len(inputHistory)-1] == "convert" {
 			if input == "y" {
 				inputHistory = append(inputHistory, input)
 				fmt.Println("Genererer filen paa nytt")
-				ReadFile()
+				processFiles()
 			} else if input == "n" {
 				inputHistory = append(inputHistory, input)
 				fmt.Println("Endringer ble ikke lagret")
 			} else {
 				fmt.Println("Svar y for ja eller n for nei")
 			}
-		} else if len(inputHistory) > 0 && inputHistory[len(inputHistory)-1] == "average" {
+		} else if inputHistory[len(inputHistory)-1] == "average" {
 			if input == "c" {
-    				inputHistory = append(inputHistory, input)
+				inputHistory = append(inputHistory, input)
 				fmt.Println("Printer ut gjennomsnittstemperatur i Celsius")
 			} else if input == "f" {
-	    			inputHistory = append(inputHistory, input)
+				inputHistory = append(inputHistory, input)
 				fmt.Println("Printer ut gjennomsnittstemperatur i Fahrenheit")
 			} else {
 				fmt.Println("Svar c eller f")
@@ -60,63 +49,78 @@ func main(){
 			fmt.Println("Konverterer alle maalingene fra Celsius til Fahrenheit")
 			if _, err := os.Stat("kjevik-temp-fahr-20220318-20230318.csv"); err == nil {
 				fmt.Println("Filen med Fahrenheit maalinger eksisterer allerede. Vil du erstatte den?")
+				logError(err)
 			} else {
 				fmt.Println("Genererer fil")
-				ReadFile()
+				processFiles()
 			}
 		} else if input == "average" {
 			inputHistory = append(inputHistory, input)
 			fmt.Println("Genererer temperaturgjennomsnittet for hele perioden. Skal det staa i Celsius 'c' eller Fahrenheit 'f'?")
-    		} else {
-        		fmt.Println("Venligst skriv convert, average eller exit")
-    		}
+		} else {
+			fmt.Println("Venligst skriv convert, average eller exit")
+		}
 	}
+}
+
+func logError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func processFiles() {
+	src, err := os.Open("kjevik-temp-celsius-20220318-20230318.csv")
+	logError(err)
+	defer src.Close()
+
+	newFile, err := os.Create("kjevik-temp-fahr-20220318-20230318.csv")
+	logError(err)
+	defer newFile.Close()
 
 	var buffer []byte
 	var linebuf []byte
-	var newLine string
 	buffer = make([]byte, 1)
 
-	bytesCount := 0
-	linesCount := 0
+	byteCount := 0
+	lineCount := 0
 
-func ReadFile() {
 	for {
-		_, err := src.Read(buffer)
-		if err != nil && err != io.EOF {
-			log.Fatal(err)
+		_, err := src.Read(buffer) // Leser av ett og ett tegn
+		if err != io.EOF {
+			logError(err)
 		}
+		byteCount++
 
-		bytesCount++
-		if buffer[0] == 0x0A {
-			linesCount++;
-			elementArray := strings.Split(string(linebuf), ";")
+		if buffer[0] != 0x0A { // Frem til tegnet for linjeskift, legges hvert tegn til i linebuf
+			linebuf = append(linebuf, buffer[0])
+		} else { // Ved linjeskift velges det hva som skal skrives til den nye filen og linebuf nullstilles
+			lineCount++
+
+			elementArray := strings.Split(string(linebuf), ";") // Deler linjen opp i "ruter" ved semikolon (;)
 			if len(elementArray) > 3 {
-				if linesCount > 1 {
-					if len(elementArray[3]) == 0 {
-						newLine = "Data er basert paa gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET);endringen er gjort av Tobias Molland;;"
-						newFile.Write([]byte(newLine))
-					} else {
-						celsius, err := strconv.ParseFloat(elementArray[3], 64)
-						if err != nil {
-							log.Fatal(err)
-						}
-							fahr := conv.CelsiusToFahrenheit(celsius)
-							newLine = elementArray[0] + ";" + elementArray[1] + ";" + elementArray[2] + ";" + fahr + "\n"
-							newFile.Write([]byte(newLine))
-					}
-				} else {
-					newLine = string(linebuf) + "\n"
-					newFile.Write([]byte(newLine))
-				}
+				newFile.Write(writeToFile(lineCount, elementArray, linebuf))
 			}
 			linebuf = nil
-		} else {
-			linebuf = append(linebuf, buffer[0])
 		}
+
 		if err == io.EOF {
 			break
 		}
 	}
 }
+
+func writeToFile(lineCount int, elementArray []string, linebuf []byte) []byte {
+	var newLine string
+	if lineCount == 1 { // Første linje forblir lik
+		newLine = string(linebuf) + "\n"
+	} else if len(elementArray[3]) != 0 { // Sjekker om tredje rute har innhold
+		celsius, err := strconv.ParseFloat(elementArray[3], 64)
+		logError(err)
+		fahr := conv.CelsiusToFahrenheit(celsius)
+		newLine = elementArray[0] + ";" + elementArray[1] + ";" + elementArray[2] + ";" + fahr + "\n"
+	} else { // Siste linje blir erstattet i dette tilfellet
+		newLine = "Data er basert paa gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET);endringen er gjort av Tobias Molland;;"
+	}
+	return []byte(newLine)
 }
