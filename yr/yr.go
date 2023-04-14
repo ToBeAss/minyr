56d2b343dec749f36e3cb4240249f121293a1322
ToBeAss <tobiasmol@uia.no>
2023-04-14 13:57:13 +0200

    la til siste tester

--- yr/yr.go
+++ yr/yr.go
@@ -1,8 +1,10 @@
 package yr
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
@@ -42,4 +44,44 @@ func CelsiusToFahrenheitLine(line string) (string, error) {
 		return "", errors.New("linje har ikke forventet format")
 	}
 	return strings.Join(dividedString, ";"), nil
+}
+
+func GetNumberOfLines(filename string) (int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func GetLastLine(filename string) (int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return 0, err
+	}
+
+	return count, nil
 }
--- yr/yr_test.go
+++ yr/yr_test.go
@@ -1,6 +1,13 @@
 package yr
 
 import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -44,5 +51,91 @@ func TestCelsiusToFahrenheitLine(t *testing.T) {
 			t.Errorf("expected %s, got: %s", tc.want, got)
 		}
 	}
+}
 
+func testNumberOfLines(t *testing.T, filename string, expected int) {
+	count, err := GetLastLine(filename)
+	if err != nil {
+		t.Fatalf("Feil ved telling av linjer: %v", err)
+	}
+
+	if count != expected {
+		t.Errorf("Uventet antall linjer i filen %s: Forventa %d, Fikk %d", filename, expected, count)
+	}
+}
+
+const inputFile = "../kjevik-temp-celsius-20220318-20230318.csv"
+const outputFile = "../kjevik-temp-fahr-20220318-20230318.csv"
+
+func TestNumberOfLines(t *testing.T) {
+	//Tester hvor mange linjer, både på input(cels) filen og output(fahr) filen
+	testNumberOfLines(t, inputFile, 16756)
+
+	testNumberOfLines(t, outputFile, 16756)
+
+}
+
+func TestCalculateAverageTemperature(t *testing.T) {
+	expected := 8.56
+	actual, err := calculateAverageTemperature(inputFile, "C")
+
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+
+	if math.Abs(actual-expected) > 0.01 {
+		t.Errorf("Expected average temperature to be %v, but got %v", expected, actual)
+	}
+}
+
+func calculateAverageTemperature(filepath, unit string) (float64, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		fmt.Println("Filen finnes ikke. Prøv først å konvertere fra celsius til farhenheit")
+		fmt.Println("Avslutter programmet.")
+		return 0, err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	var sum float64
+	var count int
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return 0, err
+		}
+
+		// Split the line into parts
+		parts := strings.Split(strings.TrimSpace(line), ";")
+		if len(parts) != 4 {
+			// Skip line if it does not contain four parts
+			fmt.Printf("Skipping line: %s", line)
+			continue
+		}
+
+		// Parse the temperature
+		temperature, err := strconv.ParseFloat(parts[3], 64)
+		if err != nil {
+			// Skip line if conversion fails
+			fmt.Printf("Skipping line: %s", line)
+			continue
+		}
+
+		// Add temperature to sum
+		sum += temperature
+		count++
+	}
+
+	// Calculate average temperature
+	var average float64
+	if count > 0 {
+		average = sum / float64(count)
+	}
+
+	return average, nil
 }
