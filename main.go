e0d535482be63877c08935df7d8cf27308c3abdb
ToBeAss <tobiasmol@uia.no>
2023-04-14 00:52:34 +0000

    Lagt til GUI i terminal

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"os"
-	"log"
+	"bufio"
 	"io"
+	"log"
+	"fmt"
 	"strings"
 	"strconv"
 	"github.com/ToBeAss/funtemps/conv"
@@ -22,6 +24,54 @@ func main(){
 	}
 	defer newFile.Close()
 
+	var input string
+	var inputHistory []string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+    		input = scanner.Text()
+    		if input == "q" || input == "exit" {
+    			inputHistory = append(inputHistory, input)
+			os.Exit(0)
+    		}
+		if len(inputHistory) > 0 && inputHistory[len(inputHistory)-1] == "convert" {
+			if input == "y" {
+				inputHistory = append(inputHistory, input)
+				fmt.Println("Genererer filen paa nytt")
+				ReadFile()
+			} else if input == "n" {
+				inputHistory = append(inputHistory, input)
+				fmt.Println("Endringer ble ikke lagret")
+			} else {
+				fmt.Println("Svar y for ja eller n for nei")
+			}
+		} else if len(inputHistory) > 0 && inputHistory[len(inputHistory)-1] == "average" {
+			if input == "c" {
+    				inputHistory = append(inputHistory, input)
+				fmt.Println("Printer ut gjennomsnittstemperatur i Celsius")
+			} else if input == "f" {
+	    			inputHistory = append(inputHistory, input)
+				fmt.Println("Printer ut gjennomsnittstemperatur i Fahrenheit")
+			} else {
+				fmt.Println("Svar c eller f")
+			}
+		} else if input == "convert" {
+			inputHistory = append(inputHistory, input)
+			fmt.Println("Konverterer alle maalingene fra Celsius til Fahrenheit")
+			if _, err := os.Stat("kjevik-temp-fahr-20220318-20230318.csv"); err == nil {
+				fmt.Println("Filen med Fahrenheit maalinger eksisterer allerede. Vil du erstatte den?")
+			} else {
+				fmt.Println("Genererer fil")
+				ReadFile()
+			}
+		} else if input == "average" {
+			inputHistory = append(inputHistory, input)
+			fmt.Println("Genererer temperaturgjennomsnittet for hele perioden. Skal det staa i Celsius 'c' eller Fahrenheit 'f'?")
+    		} else {
+        		fmt.Println("Venligst skriv convert, average eller exit")
+    		}
+	}
+
 	var buffer []byte
 	var linebuf []byte
 	var newLine string
@@ -29,6 +79,8 @@ func main(){
 
 	bytesCount := 0
 	linesCount := 0
+
+func ReadFile() {
 	for {
 		_, err := src.Read(buffer)
 		if err != nil && err != io.EOF {
@@ -66,4 +118,5 @@ func main(){
 			break
 		}
 	}
+}
 }
